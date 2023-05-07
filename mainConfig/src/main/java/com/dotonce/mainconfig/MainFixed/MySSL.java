815a3130bc246fc83d60463327ded09bb5e3efdb
Hussein <pubalayan@gmail.com>
2023-05-07 12:25:21 +0300

    Last version

---
+++ mainConfig/src/main/java/com/dotonce/mainconfig/MainFixed/MySSL.java
@@ -0,0 +1,29 @@
+package com.dotonce.mainconfig.MainFixed;
+
+import android.content.Context;
+
+import com.android.volley.toolbox.HurlStack;
+import com.dotonce.mainconfig.R;
+
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+public class MySSL {
+    public static HurlStack getCert(Context context){
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(context.getResources().openRawResource(R.raw.a), "password".toCharArray());
+            trustManagerFactory.init(keyStore);
+            sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+            return new HurlStack(null, sslContext.getSocketFactory());
+        }catch (Exception | Error ignored){}
+
+
+        return null;
+    }
+}
